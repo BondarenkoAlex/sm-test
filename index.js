57051d9ba34f6178bd0ee219548f2dd9c3ca6837
bondarenko <bondarenkoalexgit@mail.ru>
2024-09-03 14:00:29 +0300

    fix(deps): tra
    Signed-off-by: Default One

---
+++ index.js
@@ -0,0 +1,6 @@
+export const MY_CONST = "Hello world fix";
+
+export const MY_CONST_2 = "Hello world 2";
+export const MY_CONST_Local = "Hello world local";
+
+export const MY_CONST_Local_test = "Hello world local";
