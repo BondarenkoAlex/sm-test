e1fc755327931d7a5ec0330c5c6f01205337b526
bondarenko <bondarenkoalexgit@mail.ru>
2024-09-03 13:51:26 +0300

    chore: init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+export const MY_CONST = "Hello world fix";
+
+export const MY_CONST_2 = "Hello world 2";
+export const MY_CONST_Local = "Hello world local";
+
+export const MY_CONST_Local_test = "Hello world local";
+
+/*
+[
+  'lower-case', // default
+  'upper-case', // UPPERCASE
+  'camel-case', // camelCase
+  'kebab-case', // kebab-case
+  'pascal-case', // PascalCase
+  'sentence-case', // Sentence case
+  'snake-case', // snake_case
+  'start-case', // Start Case
+];
+* */
