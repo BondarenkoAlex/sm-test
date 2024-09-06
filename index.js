3601f5279d492ef6e58caa3c0ce0fa32e34b8d48
bondarenko <bondarenkoalexgit@mail.ru>
2024-09-06 20:04:35 +0300

    fix(deps): save

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ export const MY_CONST_2 = "Hello world 2";
 export const MY_CONST_Local = "Hello world local";
 
 export const MY_CONST_Local_test = "Hello world local";
+
+/*
+[
+  'lower-case', // default
+  'upper-case', // UPPERCASE
+  'camel-case', // camelCase
+  'kebab-case', // kebab-case
+  'pascal-case', // PascalCase
+  'sentence-case', // Sentence case
+  'snake-case', // snake_case
+  'start-case', // Start Case
+];
+* */
